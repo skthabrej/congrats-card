748d3c2103e980bd87d678a9c738739625b8306b
Shaik Thabrej Basha <skthabrej@gmail.com>
2022-10-08 16:42:20 +0530

    css changes

--- src/components/CongratsCard/CongratsCard.js
+++ src/components/CongratsCard/CongratsCard.js
@@ -1,21 +1,21 @@
 import React from "react";
 import { Component } from "react";
+import './CongratsCard.css'
 
 class CongratsCard extends Component {
     render() {
         return (
-            <div>
-                <div className="bg-container">
+            <div className="bg-container">
+                <div>
                     <h1>Congratulations</h1>
                 </div>
                 <div className="congrats-card">
                     <div>
                         <img src="https://i.ibb.co/LkTRMb1/tobey-maguire-joins-avengers-kang-dynasty-as-spider-man-00001.jpg"/>
                     </div>
-                    <h1>Tobey Maguire</h1>
-                    <p>Tobey Maguire is an American actor and producer. He gained international prominence for his role as Peter Parker / Spider-Man in the Spider-Man trilogy, which was directed by Sam Raimi, as well as the Marvel Cinematic Universe film Spider-Man: No Way Home.</p>
+                    <h1 className="name">Tobey Maguire</h1>
+                    <p className="para">Tobey Maguire is an American actor and producer. He gained international prominence for his role as Peter Parker / Spider-Man in the Spider-Man trilogy, which was directed by Sam Raimi, as well as the Marvel Cinematic Universe film Spider-Man: No Way Home.</p>
                 </div>
-
             </div>
         )
     }
